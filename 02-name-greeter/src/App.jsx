688dd44bd0648c47906b3d2c6c125bace2e8efb9
Aamna <aash00001@stud.uni-saarland.de>
2026-09-14 20:40:32 +0200

    03-character-counter initial commit

--- 02-name-greeter/src/App.jsx
+++ 02-name-greeter/src/App.jsx
@@ -6,20 +6,21 @@ import './App.css'
 
 function App() {
 const [name, setName] = useState("");
+
   return (
     <>
-    <div>
-      <h1> Name Greeter </h1>
-      <input type="text" 
-      placeholder='Enter your name'
-      value={name}
-      onChange={(e)=>{
-        setName(e.target.value)
-      }}
-      >
-      </input>
-      <h2>Hello {name || "Stranger"}!</h2>
-    </div>
+   <div>
+    <h1>Name Changer with React</h1>
+
+    <input 
+    type='text'
+    placeholder='Enter Your Name'
+    value={name}
+    onChange={(e)=>{setName(event.target.value)}}></input>
+
+    <h2> Hello {name || "Stranger" }!</h2>
+   </div>
+
     <footer>
       <p>Made with ❤️ by <a href="https://aamnashahab.com" target="_blank">Aamna Shahab</a></p>
     </footer>
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 learning react with multiple small projects for brushing up concepts <br>
 Project 1: 01-counter - counter using useState, onClick <br>
 Project 2: 02-name-greeter - event.target.value, onChange <br>
+ 

6a03d74eb0a919c51f46cbabf9c7b0b88826e8ce
Aamna <aash00001@stud.uni-saarland.de>
2026-09-15 15:56:04 +0200

    05 pass toggle setTimeout added

--- 05-password-toggle/src/App.jsx
+++ 05-password-toggle/src/App.jsx
@@ -5,7 +5,18 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
+  const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setShowPassword(true); 
+
+    setTimeout(() => {
+      setShowPassword(false);
+    }, 250);
+  };
+  
 
   return (
     <>
@@ -13,19 +24,18 @@ function App() {
         <h1> Password Toggle with React </h1>
         <input 
         type={showPassword ? "text" : "password" }
-        placeholder='Enter password'
-        onChange={(e) => setCount(e.target.value)} >
+        value={password}
+        onChange={handlePasswordChange}
+        placeholder='Enter password'>
         </input> 
-
-        <br></br>
         <button onClick={() => setShowPassword(!showPassword)}>
           {showPassword ? "Hide" : "Show"} Password
         </button>
-        
-      
-
-
       </div>
+
+      <footer style={{position: "fixed", bottom: "0", width: "100%", textAlign: "center"}}>
+      <p>Made with ❤️ by <a href="https://aamnashahab.com" target="_blank" rel="noopener noreferrer">Aamna Shahab</a></p>
+    </footer>
     </>
   )
 }

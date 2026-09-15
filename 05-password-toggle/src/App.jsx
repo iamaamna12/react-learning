a4af3a4314b9c6f3f38fbfd92e7b21acdafc1234
Aamna <aash00001@stud.uni-saarland.de>
2026-09-15 14:34:30 +0200

    05 password toggle base

---
+++ 05-password-toggle/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <>
+      <div>
+        <h1> Password Toggle with React </h1>
+        <input 
+        type={showPassword ? "text" : "password" }
+        placeholder='Enter password'
+        onChange={(e) => setCount(e.target.value)}  
+
+        />
+
+
+      </div>
+    </>
+  )
+}
+
+export default App
